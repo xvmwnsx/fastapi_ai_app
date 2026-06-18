4820a687dd483a5ca456fb0c6f9ea3b1815d4b12
ksenia <kochetkovaksenia1@gmail.com>
2026-06-18 09:39:16 +0300

    добавлено подключение и таблица в бд sqlite запросов пользователя; добавлен фронтенд страницы чата; добавлена обработка запроса в main.py;

--- db.py
+++ db.py
@@ -1,4 +1,32 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import sessionmaker, DeclarativeBase, Mapped, mapped_column
 
-engine = create_engine(url="sqlite:///chat_requests.db")
+engine = create_engine(url="sqlite:///requests.db")
+
+session = sessionmaker(engine)
+
+class Base(DeclarativeBase):
+    pass
+
+class ChatRequests(Base):
+    __tablename__ = "chat_requests"
+    id: Mapped[int] = mapped_column(primary_key = True)
+    ip_address: Mapped[str] = mapped_column(index = True)
+    prompt: Mapped[str]
+    response: Mapped[str]
+    
+def get_user_requests(ip_address: str) -> list[ChatRequests]:
+    with session() as new_session:
+        query = select(ChatRequests).filter_by(ip_address = ip_address)
+        result = new_session.execute(query)
+        return result.scalars().all()
+
+def add_request_data(ip_address: str, prompt: str, response: str) -> None:
+    with session() as new_session:
+        new_request = ChatRequests(
+            ip_address = ip_address,
+            prompt = prompt,
+            response = response,
+        )
+        new_session.add(new_request)
+        new_session.commit()
--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-from fastapi import FastAPI, Body
+from contextlib import asynccontextmanager
+from fastapi import FastAPI, Body, Request
+from fastapi.middleware.cors import CORSMiddleware
 from grop_client import get_response
+from db import Base, engine, get_user_requests, add_request_data
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    Base.metadata.create_all(engine)
+    print("Все таблицы созданы")
+    yield
+
+app = FastAPI(
+    title = "Мой стартап",
+    lifespan = lifespan
+    )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5500", "http://127.0.0.1:5500"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/requests")
-def get_requests():
-    return "Hello World"
+def get_requests(request: Request):
+    user_ip_address = request.client.host
+    print(f"{user_ip_address=}")
+    user_requests = get_user_requests(ip_address = user_ip_address)
+    return [
+        {"id": r.id, "prompt": r.prompt, "response": r.response}
+        for r in user_requests
+    ]
 
 @app.post("/requests")
 def send_prompt(
-    input: str = Body(embed=True)
+    request: Request,
+    prompt: str = Body(embed=True),
 ):
-    answer = get_response(input)
-    return {"answer": answer}
-    
+    user_ip_address = request.client.host
+    answer = get_response(prompt)
+    add_request_data(
+        ip_address = user_ip_address,
+        prompt = prompt,
+        response = answer,
+    )
+    return {"answer": answer}    
